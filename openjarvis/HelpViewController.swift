198ad502be729b4e4dc4e1119857bc34fa56186c
SebastienVProject <vergnollesebastien@icloud.com>
2017-06-13 00:49:16 +0200

    Ecran d'aide renseigné, et formulaire de contact mis en place Reste à faire un envoi de mail des que le bouton envoyer est tapé

--- openjarvis/HelpViewController.swift
+++ openjarvis/HelpViewController.swift
@@ -8,12 +8,23 @@
 
 import UIKit
 
-class HelpViewController: UIViewController {
+class HelpViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate, UITextFieldDelegate, UITextViewDelegate {
 
     @IBOutlet weak var imageJarvisHelp: UIImageView!
+    @IBOutlet weak var pickerViewType: UIPickerView!
+    
+    @IBOutlet weak var textViewMessage: UITextView!
+    @IBOutlet weak var textFieldEmail: UITextField!
+    
+    var pickerDataSource = ["Question", "Encouragement", "Evolution", "Bug"]
+    var emailSaisiOK: Bool = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        pickerViewType.dataSource = self
+        pickerViewType.delegate = self
+        textViewMessage.delegate = self
 
         let blurEffect = UIBlurEffect(style: UIBlurEffectStyle.dark)
         let blurEffectView = UIVisualEffectView(effect: blurEffect)
@@ -27,7 +38,43 @@ class HelpViewController: UIViewController {
     
     }
     
-
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerDataSource.count;
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return pickerDataSource[row]
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        self.view.endEditing(true)
+        
+        return true
+        
+    }
+    
+    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+        if(text == "\n") {
+            textView.resignFirstResponder()
+            return false
+        }
+        return true
+    }
+    
+    func isValidEmail(emailSaisi:String) -> Bool {
+        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}"
+        let emailTest = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+        return emailTest.evaluate(with: emailSaisi)
+    }
+    
+    @IBAction func ValidationFormulaire(_ sender: UIButton) {
+        emailSaisiOK = isValidEmail(emailSaisi: textFieldEmail.text!)
+    }
     /*
     // MARK: - Navigation
 
